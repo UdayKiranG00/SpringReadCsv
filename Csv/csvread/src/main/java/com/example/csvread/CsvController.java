835db8968dfbdb295d35e3be60d56fe90597f867
UdayKiran <udayganguru2000@gmail.com>
2023-10-11 23:41:13 +0530

    sent user data from postman and tested..

---
+++ Csv/csvread/src/main/java/com/example/csvread/CsvController.java
@@ -0,0 +1,26 @@
+package com.example.csvread;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsvController {
+	
+	@Autowired
+	CsvRepository csvRepo;
+	
+	@GetMapping("/hello")
+	public String saySomething() {
+		return "hi hello";
+	}
+	
+	@PostMapping("/user")
+	public String saveUser(@RequestBody Users users) {
+		csvRepo.save(users);
+		return "saved succesfully";
+	}
+}
