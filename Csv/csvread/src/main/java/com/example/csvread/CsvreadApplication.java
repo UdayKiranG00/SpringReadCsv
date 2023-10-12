60783fff05a7b4b46a1ec4217f6e87a132f553fe
uday kiran <udayganguru2000@gmail.com>
2023-10-12 10:49:28 +0530

    tested csv file ,worked well.

--- Csv/csvread/src/main/java/com/example/csvread/CsvreadApplication.java
+++ Csv/csvread/src/main/java/com/example/csvread/CsvreadApplication.java
@@ -1,32 +1,39 @@
 package com.example.csvread;
 
+import java.io.FileReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.opencsv.CSVReader;
 
 @SpringBootApplication
 public class CsvreadApplication {
 
-//	static
-//	CsvRepository csvRepo;
-//	
-//	public CsvreadApplication(CsvRepository csvRepo) {
-//		CsvreadApplication.csvRepo=csvRepo;
-//	}
+	static
+	CsvRepository csvRepo;
+	
+	public CsvreadApplication(CsvRepository csvRepo) {
+		CsvreadApplication.csvRepo=csvRepo;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CsvreadApplication.class, args);
-//		try(CSVReader reader=new CSVReader(new FileReader("src/main/resources/user.csv"))){
-//			String[] nextLine=null;
-//			while((nextLine=reader.readNext())!=null) {
-//				Users u =new Users();
-//				u.setId(Integer.parseInt(nextLine[0]));
-//				u.setName(nextLine[1]);
-//				u.setAge(Integer.parseInt(nextLine[2]));
-//				csvRepo.save(u);
-//			}
-//		}
-//		catch(Exception e) {
-//			
-//		}
+		System.out.println("hi");
+		try(CSVReader reader=new CSVReader(new FileReader("src/main/resources/user.csv"))){
+			String[] nextLine=null;
+			System.out.println("NEXT");
+			while((nextLine=reader.readNext())!=null) {
+				Users u =new Users();
+				u.setId(Integer.parseInt(nextLine[0]));
+				System.out.println("id is : "+nextLine[0]);
+				u.setName(nextLine[1]);
+				u.setAge(Integer.parseInt(nextLine[2]));
+				csvRepo.save(u);
+			}
+		}
+		catch(Exception e) {
+			
+		}
 	}
 
 }
